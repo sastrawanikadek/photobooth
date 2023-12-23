3bcf74002aeb2eb386d38f161b8ec619720a5db8
Kadek Sastrawan <sastrawanikadek@gmail.com>
2023-12-23 06:19:17 +0000

    refactor: add commitlint and no-commit-to-branch hooks in pre-commit (#1)

---
+++ commitlint.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  /**
+   * Resolve and load @commitlint/config-conventional from node_modules.
+   * Referenced packages must be installed
+   */
+  extends: ["@commitlint/config-conventional"],
+  /**
+   * Resolve and load @commitlint/format from node_modules.
+   * Referenced package must be installed
+   */
+  formatter: "@commitlint/format",
+  /**
+   * Any rules defined here will override rules from @commitlint/config-conventional
+   */
+  rules: {
+    "scope-enum": [
+      2,
+      "always",
+      [
+        "server",
+        "component_manager",
+        "eventbus",
+        "injector",
+        "utils",
+        "websocket",
+      ],
+    ],
+    "references-empty": [2, "never"],
+  },
+};
