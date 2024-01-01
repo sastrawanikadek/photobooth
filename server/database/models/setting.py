cf309abbe3e22293ef909c0abb30ea7ab2581d73
ryan <ryanpradnya@gmail.com>
2024-01-01 13:14:19 +0000

    refactor(database): update setting model (#5)

--- server/database/models/setting.py
+++ server/database/models/setting.py
@@ -1,19 +1,44 @@
+from datetime import datetime
+
+from sqlalchemy import func
 from sqlmodel import Field, SQLModel
 
+from server.utils.pydantic_fields import SlugStr
 
-class Setting(SQLModel):
+
+# Ignore mypy error until SQLModel fixed it
+class Setting(SQLModel, table=True):  # type: ignore
+    """
+    Model and schema for a setting.
+    """
+
+    __tablename__ = "settings"
+
     id: int = Field(primary_key=True)
-    slug: str
+    slug: SlugStr
     key: str
     type: str
-    _value: str
+    value: str
+    created_at: datetime = Field(sa_column_kwargs={"server_default": func.now()})
+    updated_at: datetime = Field(
+        sa_column_kwargs={"server_default": func.now(), "onupdate": func.now()},
+    )
 
-    def value(self) -> str | bool | int | float:
+    @property
+    def parsed_value(self) -> str | bool | int | float:
+        """
+        Parse the value of the setting.
+
+        Returns
+        -------
+        str | bool | int | float
+            The parsed value of the setting.
+        """
         if self.type == "boolean":
-            return self._value == "true"
+            return self.value == "true"
         elif self.type == "integer":
-            return int(self._value)
+            return int(self.value)
         elif self.type == "float":
-            return float(self._value)
+            return float(self.value)
 
-        return self._value
+        return self.value
