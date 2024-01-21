cee2bb5507a51a006d0d584d0c40390e92cb152d
Kadek Sastrawan <sastrawanikadek@gmail.com>
2024-01-21 22:02:28 +0800

    feat(server): add service provider (#21)
    Service provider used as the central place of registering dependencies, event listeners, etc

--- server/interfaces.py
+++ server/interfaces.py
@@ -1,8 +1,9 @@
 from abc import ABC
+from typing import Type
 
-from eventbus import EventBusInterface
-from injector import DependencyContainerInterface, DependencyInjectorInterface
-from managers.component import ComponentManagerInterface
+from server.eventbus import Event, EventBusInterface, Listener
+from server.injector import DependencyContainerInterface, DependencyInjectorInterface
+from server.managers.component import ComponentManagerInterface
 
 
 class PhotoboothInterface(ABC):
@@ -25,3 +26,46 @@ class PhotoboothInterface(ABC):
     dependency_container: DependencyContainerInterface
     dependency_injector: DependencyInjectorInterface
     eventbus: EventBusInterface
+
+
+class ServiceProviderInterface:
+    """Interface for service providers."""
+
+    dependencies: dict[type, type] = {}
+    dependency_container: DependencyContainerInterface
+    eventbus: EventBusInterface
+    singletons: dict[type, type] = {}
+    event_listeners: dict[Type[Event], list[Listener]] = {}
+
+    def __init__(
+        self,
+        dependency_container: DependencyContainerInterface,
+        eventbus: EventBusInterface,
+    ) -> None:
+        """
+        Initialize the service provider.
+
+        Parameters
+        ----------
+        dependency_container : DependencyContainerInterface
+            The dependency container.
+        eventbus : EventBusInterface
+            The eventbus.
+        """
+        self.dependency_container = dependency_container
+        self.eventbus = eventbus
+
+    def register(self) -> None:
+        """
+        Register the service provider.
+
+        This method is called on the preparation lifecycle stage.
+        """
+
+    def boot(self) -> None:
+        """
+        Boot the service provider.
+
+        This method is called on the startup lifecycle stage and
+        most of the dependencies should be available to be injected.
+        """
