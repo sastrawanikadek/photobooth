b6483b73ca7e54d5d5626606d18deae71a07402b
Kadek Sastrawan <37144427+sastrawanikadek@users.noreply.github.com>
2024-03-16 00:52:28 +0800

    build: update tasks and setup script also remove devcontainer (#30)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,11 +7,12 @@ fi
 
 source .venv/bin/activate
 
-
 pip3 install -r requirements-dev.txt
 pip3 install -r requirements.txt
 
 python3 -m scripts.generate_requirements
 pip3 install -r requirements-components.txt
+
+yarn install
 
 pre-commit install
